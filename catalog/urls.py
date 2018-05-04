e5635d36fe25321ff2d8734ef609c98eef7563f8
Khalamkhan Fariza <khalamkhanf@gmail.com>
2018-05-04 10:46:24 +0600

    first commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path(r'^spisokmenus/$', views.SpisokMenuListView.as_view(), name='spisokmenus'),
+    path('spisokmenus/', views.SpisokMenuListView.as_view(), name='spisokmenus'),
     path('spisokmenu/<int:pk>', views.SpisokMenuDetailView.as_view(), name='spisokmenu-detail'),
     path('sponsors/', views.SponsorListView.as_view(), name='sponsors'),
     path('sponsor/<int:pk>', views.SponsorDetailView.as_view(), name='sponsor-detail'),
